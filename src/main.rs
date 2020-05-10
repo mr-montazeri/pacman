558dd218988aa495931d3efdc7c30814a174e5cd
Mohammadreza MontazeriShatoori <monta2009@gmail.com>
2020-05-10 20:29:54 -0300

    Fix bugs

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod utils;
 use crate::world::{WorldModel, Position};
 use std::collections::{HashSet, VecDeque};
 use crate::pellet::Pellet;
+use std::time::Instant;
 
 fn nearest_pellet(wm: &WorldModel, start: Position) -> Option<&Pellet> {
     let mut visited = HashSet::new();
@@ -15,10 +16,16 @@ fn nearest_pellet(wm: &WorldModel, start: Position) -> Option<&Pellet> {
     to_visit.push_back(start);
 
     while let Some(pos) = to_visit.pop_front() {
+        eprintln!("At {:?}", pos);
+        visited.insert(pos);
         let neighbors = wm.neighbors(pos);
+        eprintln!("Neighbors: {:?}", neighbors);
         for cell in neighbors {
             if !visited.contains(&cell) {
+                eprintln!("Will visit: {:?}", cell);
                 to_visit.push_back(cell);
+            } else {
+                eprintln!("Already visited: {:?}", cell);
             }
         }
         if let Some(pellet) = wm.pellet_at(pos) {
@@ -36,18 +43,30 @@ fn main() {
 
     // game loop
     loop {
+        let start = Instant::now();
         wm.update_by_input();
+
+        let mut result = Vec::new();
 
         for pac in wm.get_team_pacs() {
             // Finding closest pellet
+            eprintln!("Pac {} at {:?}", pac.id(), pac.pos());
             match nearest_pellet(&wm, pac.pos()) {
                 None => {
-                    println!("MOVE {} {} {}", pac.id(), pac.pos().0, pac.pos().1);
+                    eprintln!("Cant find pellet :(");
+                    result.push(format!("MOVE {} {} {}", pac.id(), pac.pos().0, pac.pos().1))
                 }
                 Some(pellet) => {
-                    println!("MOVE {} {} {}", pac.id(), pellet.pos().0, pellet.pos().1);
+                    eprintln!("Going to {:?}", pellet.pos());
+                    result.push(format!("MOVE {} {} {}", pac.id(), pellet.pos().0, pellet.pos().1));
                 }
             }
         }
+        let result = result.join(" | ");
+        println!("{}", result);
+
+        let duration = start.elapsed();
+
+        eprintln!("Time elapsed in step is: {:?}", duration);
     }
 }
--- src/world.rs
+++ src/world.rs
@@ -9,7 +9,7 @@ use crate::pellet::Pellet;
 #[repr(u8)]
 #[derive(Debug)]
 pub enum CellType {
-    Wall = b'W',
+    Wall = b'#',
     Floor = b' ',
 }
 
@@ -25,7 +25,7 @@ impl TryFrom<u8> for CellType {
     }
 }
 
-pub type Position = (usize, usize);
+pub type Position = (isize, isize);
 
 #[derive(Clone, Copy, Hash, Eq, PartialEq, Debug)]
 pub enum Team {
@@ -42,7 +42,7 @@ pub type PacIdentifier = (Team, i32);
 
 #[derive(Default)]
 pub struct WorldModel {
-    dim: (usize, usize),
+    dim: (isize, isize),
     grid: Vec<CellType>,
     pacs: HashMap<PacIdentifier, Pac>,
     pellets: HashMap<Position, Pellet>,
@@ -56,25 +56,26 @@ impl WorldModel {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let width = parse_input!(inputs[0], usize); // size of the grid
-        let height = parse_input!(inputs[1], usize); // top left corner is (x=0, y=0)
+        let width = parse_input!(inputs[0], isize); // size of the grid
+        let height = parse_input!(inputs[1], isize); // top left corner is (x=0, y=0)
 
         let mut cells: Vec<CellType> = Vec::new();
 
         for _ in 0..height {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
-            let row = input_line.trim_end(); // one line of the grid: space " " is floor, pound "#" is wall
-            for c in row.as_bytes() {
-                cells.push(CellType::try_from(*c).unwrap());
+            for c in input_line.as_bytes() {
+                if let Ok(cell_type) = CellType::try_from(*c) {
+                    cells.push(cell_type);
+                }
             }
         }
 
         Self::new((width, height), cells)
     }
 
-    pub fn new(dim: (usize, usize), grid: Vec<CellType>) -> Self {
-        assert_eq!(dim.0 * dim.1, grid.len());
+    pub fn new(dim: (isize, isize), grid: Vec<CellType>) -> Self {
+        assert_eq!(dim.0 * dim.1, grid.len() as isize);
         WorldModel {
             dim,
             grid,
@@ -101,14 +102,14 @@ impl WorldModel {
         io::stdin().read_line(&mut input_line).unwrap();
         let visible_pac_count = parse_input!(input_line, i32); // all your pacs and enemy pacs in sight
 
-        for _ in 0..visible_pac_count as usize {
+        for _ in 0..visible_pac_count as isize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
             let id = parse_input!(inputs[0], i32); // pac number (unique within a team)
             let mine = parse_input!(inputs[1], i32) == 1; // true if this pac is yours
-            let x = parse_input!(inputs[2], usize); // position in the grid
-            let y = parse_input!(inputs[3], usize); // position in the grid
+            let x = parse_input!(inputs[2], isize); // position in the grid
+            let y = parse_input!(inputs[3], isize); // position in the grid
             // let type_id = inputs[4].trim().to_string(); // unused in wood leagues
             // let speed_turns_left = parse_input!(inputs[5], i32); // unused in wood leagues
             // let ability_cooldown = parse_input!(inputs[6], i32); // unused in wood leagues
@@ -131,12 +132,12 @@ impl WorldModel {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let visible_pellet_count = parse_input!(input_line, i32); // all pellets in sight
-        for _ in 0..visible_pellet_count as usize {
+        for _ in 0..visible_pellet_count as isize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
-            let x = parse_input!(inputs[0], usize);
-            let y = parse_input!(inputs[1], usize);
+            let x = parse_input!(inputs[0], isize);
+            let y = parse_input!(inputs[1], isize);
             let value = parse_input!(inputs[2], i32); // amount of points this pellet is worth
             let pos = (x, y);
 
@@ -148,9 +149,9 @@ impl WorldModel {
         assert!(pos.0 < self.dim.0);
         assert!(pos.1 < self.dim.1);
 
-        let strides = (self.dim.1, 1);
+        let strides = (self.dim.0, 1);
 
-        pos.0 * strides.0 + pos.1 * strides.1
+        (pos.1 * strides.0 + pos.0 * strides.1) as usize
     }
 
     pub fn kind(&self, pos: Position) -> &CellType {
@@ -161,14 +162,15 @@ impl WorldModel {
         let mut out = Vec::new();
 
         let neighbors: [Position; 4] = [
-            ((pos.0 - 1) % self.dim.0, pos.1), // Left
-            (pos.0, (pos.1 - 1) % self.dim.1), // Top
+            ((pos.0 - 1 + self.dim.0) % self.dim.0, pos.1), // Left
+            (pos.0, (pos.1 - 1 + self.dim.1) % self.dim.1), // Top
             ((pos.0 + 1) % self.dim.0, pos.1), // Right
             (pos.0, (pos.1 + 1) % self.dim.1), // Bottom
         ];
 
         for neighbor in neighbors.iter() {
             let neighbor_kind = self.kind(*neighbor);
+            // eprintln!("# neighbor at {:?}: Kind={:?}", neighbor, neighbor_kind);
             match neighbor_kind {
                 CellType::Floor => out.push(*neighbor),
                 _ => (),
